6597eb3b5f2c369e2638a637c6c6fde767c3545a
zohair khan <zohairm344@gmail.com>
2026-02-06 20:24:25 +0500

    latest

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -8,6 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
+    <div>
     <TasksProvider>
       <div className="flex min-h-screen flex-col bg-background">
           {/* Header with Centering Container */}
